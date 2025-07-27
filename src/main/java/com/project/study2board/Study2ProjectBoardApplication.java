359c422b05f8da69c67a5ab0a140700374d38828
FOREST <bye0414@naver.com>
2025-07-27 13:44:59 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/project/study2board/Study2ProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.project.study2board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study2ProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study2ProjectBoardApplication.class, args);
+	}
+
+}
